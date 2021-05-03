8a24ce131b396db426cb6b3f9687c8cf39ac5fcd
mickaelcampos <camposmrc01@gmail.com>
2021-05-03 00:59:42 -0300

    remove hello world

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,4 @@
 (async () => {
 	const app = (await import('./app')).default;
- 
-	app.get('/', function (_req, res) {
-		res.send('Hello World');
-	});
-
 	app.listen(3000, () => console.log(`Server running at http://localhost:${3000}`));
 })();
